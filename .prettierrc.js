559e9f5043d606792a2ddcb8207529da1a0fc8ae
chenbo <chenbo@huaweisoft.com>
2021-09-07 18:31:16 +0800

    路由格式化

--- .prettierrc.js
+++ .prettierrc.js
@@ -13,4 +13,6 @@ module.exports = {
   semi: false,
   // 使用单引号代替双引号
   singleQuote: true,
+  // 换行符：lf=仅换行,默认值，常见于 Linux 和 macOS 以及 git repos 内(\n);crlf=回车符加换行符，常见于Windows(\r\n);auto=依据第一行换行符维持文件的行尾
+  endOfLine: 'auto'
 }
